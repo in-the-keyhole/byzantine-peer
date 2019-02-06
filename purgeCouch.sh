cd70105c722cb4eb602d8050bc6d8b53a041463e
Thomas Kendall <thomas.j.kendall@gmail.com>
2019-02-06 13:11:23 -0600

    Copied stuff over from byzantine-flu repository

---
+++ purgeCouch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Exit on first error, print all commands.
+set -ev
+
+# don't rewrite paths for Windows Git Bash users
+export MSYS_NO_PATHCONV=1
+
+docker-compose -f  docker-compose.couch.yml down --remove-orphans
+
